1d895a87228df4d7b98ea4598fe723efdbda91bc
Stiexeno <57089388+Stiexeno@users.noreply.github.com>
2020-03-02 10:40:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,28 @@
 <?php
-    $title = 'Coin-Flip';
-    $tail = 'https://www.nicepng.com/png/full/146-1464848_quarter-tail-png-tails-on-a-coin.png';
-    $head = 'https://cdn.clipart.email/b71d324704226dd3444f0734cfc28f2d_washington-silver-quarter-obverse-coin-heads-and-tails-clip-art-_920-815.jpeg';
-    $array = array($tail, $head);
-    $result = $array[array_rand($array, 1)];
 
-    if($result == 1){
-        print 'kurwa';
-    } else if($result == 0){
-        print 'kurwa1';
-    }
+    $police_fuel = rand(1,100);
+    $police_cons = 7.5;
+    $my_fuel = rand(1,100);
+    $my_cons = 11.5;
+
+    $p_distance = round($police_fuel / $police_cons);
+    $m_distance = round($my_fuel / $my_cons);
+
+    $h1 = 'Pabegimo skaiciuokle';
+    $li_1 = "Farai nuvaziuotu: $p_distance";
+    $li_2 = "As nuvaziuociau: $m_distance";
+
 ?>
 
 <html>
-<head>
-    <title><?php print $title?></title>
-    <style>
-        .center{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-        .coin{
-            width: 300px;
-            height: 300px;
-            background: url("<?php echo $result?>");
-            background-position: center;
-            background-size: contain;
-            background-repeat: no-repeat;
-        }
-        *{
-            border: 1px solid red;
-        }
+    <head>
 
-    </style>
-</head>
-<body class="center">
-    <div class="coin">
-    </div>
-    <span>asdsd</span>
-</body>
+    </head>
+    <body>
+        <h1><?php print $h1 ?></h1>
+        <ul>
+            <li><?php print $li_1 ?></li>
+            <li><?php print $li_2 ?></li>
+        </ul>
+    </body>
 </html>
